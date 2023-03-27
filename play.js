a1da84cae390dd5be0e8aa2166dc0521a6c4da01
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2023-03-27 17:44:50 -0300

    Initial commit

---
+++ play.js
@@ -0,0 +1,23 @@
+const card = document.querySelector('.card__inner');
+const card02 = document.querySelector('.card__inner02');
+const card03 = document.querySelector('.card__inner03');
+
+
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+    
+});
+
+card02.addEventListener('click', function () {
+    card02.classList.toggle('is-flipped');
+    
+});
+    
+
+card03.addEventListener('click', function () {
+    card03.classList.toggle('is-flipped');
+    
+});
+    
+
+
